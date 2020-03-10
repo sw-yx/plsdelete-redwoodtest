069645b7609ad53d3652dffa96d6f6cf6c3114ae
swyx <shawnthe1@gmail.com>
2020-03-10 13:25:49 -0400

    init

---
+++ web/src/pages/HomePage/HomePage.js
@@ -0,0 +1,9 @@
+// web/src/pages/HomePage/HomePage.js
+
+import BlogLayout from 'src/layouts/BlogLayout'
+
+const HomePage = () => {
+  return <BlogLayout>Home</BlogLayout>
+}
+
+export default HomePage
